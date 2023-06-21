b1f66ef39dcbec049edfb46c01689809ace9cb25
jaeheech <ks2451ss1@naver.com>
2023-06-21 17:44:07 +0900

    MEVN

--- vuejs/v02/src/assets/productData.js
+++ vuejs/v02/src/assets/productData.js
@@ -2,13 +2,13 @@ export default [
   {
     product_name: '기계식 키보드',
     price: 25000,
-    category: '노트북/태블릿',
+    category: '액세서리',
     delivery_price: 5000
   },
   {
     product_name: '무선 마우스',
     price: 12000,
-    category: '노트북/태블릿',
+    category: '액세서리',
     delivery_price: 5000
   },
   {
@@ -18,21 +18,21 @@ export default [
     delivery_price: 10000
   },
   {
-    product_name: '태블릿 거치대',
-    price: 32000,
+    product_name: '태블릿PC',
+    price: 520000,
     category: '노트북/태블릿',
     delivery_price: 5000
   },
   {
     product_name: '블루투스이어폰',
     price: 51000,
-    category: '노트북/태블릿',
+    category: '액세서리',
     delivery_price: 5000
   },
   {
     product_name: '차량용 충전기',
     price: 72000,
-    category: '노트북/태블릿',
+    category: '액세서리',
     delivery_price: 5000
   }
 ]
