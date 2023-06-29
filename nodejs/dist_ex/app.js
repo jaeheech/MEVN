eb94cfb80afbcf9a2793b91231fafd7da7d9d6eb
jaeheech <ks2451ss1@naver.com>
2023-06-29 17:31:49 +0900

    시험문제

--- nodejs/dist_ex/app.js
+++ nodejs/dist_ex/app.js
@@ -15,17 +15,18 @@ app.use(logger('tiny'))
 
 app.get('/test', (req, res) => {
   let id2 = req.query.id
-  let url_name = +req.query.name
+  let url_name = req.query.name
 
   res.send('아이디:' + id2 + '이름:' + url_name)
 })
 
+/*파일 작성 */
 app.post('/info', (req, res) => {
   const obj = req.body
   console.log(obj.name)
   console.log(obj.content)
-  console.log(obj.date)
-  fs.writeFile(_path + '/' + obj.name + '.txt', obj.content + obj.date, (e) => {
+  console.log(obj.age)
+  fs.writeFile(_path + '/' + obj.name + '.txt', obj.content + obj.age, (e) => {
     if (e) console.log(e)
     console.log('파일 작성이 완료 되었습니다.')
     res.send(
@@ -38,4 +39,3 @@ app.listen(PORT, () => {
   console.log(`서버가 ${PORT}에서 동작중!`)
 })
 console.log('현재경로:', __dirname)
-app.post('/data', (req, res) => {})
